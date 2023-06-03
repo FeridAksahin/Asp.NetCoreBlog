d614c52f1194039a06c8ddfc889e4d92ba42b352
Ferid Akşahin <ferid.aksahin98@gmail.com>
2023-06-03 18:20:33 +0300

    Revert "Remove *.cache and *.targets files from Git tracking"
    This reverts commit ebf817c6f2ec7796fb4300095ad1764fbc6c6db5.

--- Blog.API/Blog.API/Program.cs
+++ Blog.API/Blog.API/Program.cs
@@ -1,11 +1,3 @@
-using Blog.API.Entity.Context;
-using Microsoft.EntityFrameworkCore;
-
-var configuration = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json")
-    .Build();
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,8 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-string connectionString = configuration.GetConnectionString("ConnectionStringForBlog");
-builder.Services.AddDbContext<BlogContext>(x => x.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
@@ -31,5 +21,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+//
 app.Run();
