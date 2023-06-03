adabfb8fb07dcca313815bfdd6eb67cb5024d053
Ferid Akşahin <ferid.aksahin98@gmail.com>
2023-06-03 21:16:39 +0300

    Added blog ui project

---
+++ .Net Core MVC BlogMaster/Blog.BlogMaster/Blog.BlogMaster/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blog.BlogMaster.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
