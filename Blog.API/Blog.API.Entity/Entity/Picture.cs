1de61dbc6b244d9de93ae052c6a60d2c576ed10b
Ferid Akşahin <ferid.aksahin98@gmail.com>
2023-06-03 13:18:30 +0300

    [CodeFirst] Added model and context with Code First approach

---
+++ Blog.API/Blog.API.Entity/Entity/Picture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.API.Entity.Entity
+{
+    [Table("Picture")]
+    public class Picture
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ArticleId { get; set; }
+        [ForeignKey("ArticleId")]
+        public virtual Article Article { get; set; }
+
+    }
+}
--- Blog.API/Blog.API/Program.cs
+++ Blog.API/Blog.API/Program.cs
@@ -1,3 +1,11 @@
+using Blog.API.Entity.Context;
+using Microsoft.EntityFrameworkCore;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+string connectionString = configuration.GetConnectionString("ConnectionStringForBlog");
+builder.Services.AddDbContext<BlogContext>(x => x.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
