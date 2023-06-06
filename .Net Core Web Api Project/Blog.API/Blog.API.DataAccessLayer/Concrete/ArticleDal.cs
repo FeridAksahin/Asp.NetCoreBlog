e1e3b30bea5e9ebb4bbc5ca29a0d3e12bab54f35
Ferid Akşahin <ferid.aksahin98@gmail.com>
2023-06-06 22:01:56 +0300

    Added article api controller and related dal method

---
+++ .Net Core Web Api Project/Blog.API/Blog.API.DataAccessLayer/Concrete/ArticleDal.cs
@@ -0,0 +1,42 @@
+﻿using Blog.API.DataAccessLayer.Interface;
+using Blog.API.DataTransferObject;
+using Blog.API.Entity.Context;
+using Blog.API.Entity.Entity;
+
+namespace Blog.API.DataAccessLayer.Concrete
+{
+    public class ArticleDal : IArticleDal
+    {
+        private readonly BlogContext _context;
+        public ArticleDal(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddArticle(ArticleDTO article)
+        {
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    _context.Article.Add(new Article
+                    {
+                        Text = article.Text,
+                        UserId = article.UserId,
+                        CategoryId = article.CategoryId,
+                        Header = article.Header,
+                        SubHeader = article.SubHeader
+                    });
+                    await _context.SaveChangesAsync();
+                    transaction.Commit();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ .Net Core Web Api Project/Blog.API/Blog.API.DataAccessLayer/Interface/IArticleDal.cs
@@ -0,0 +1,14 @@
+﻿using Blog.API.DataTransferObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.API.DataAccessLayer.Interface
+{
+    public interface IArticleDal
+    {
+        public Task<bool> AddArticle(ArticleDTO article);
+    }
+}
